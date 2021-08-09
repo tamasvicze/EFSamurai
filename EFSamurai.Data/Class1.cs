598a48dd1b244c31080a195d998b24ea3fddb146
Tamas Vicze <tomvicze96@hotmail.com>
2021-08-09 10:31:06 +0200

    Add project files.

---
+++ EFSamurai.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EFSamurai.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ EFSamurai.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EFSamurai.Domain
+{
+    public class Class1
+    {
+    }
+}
